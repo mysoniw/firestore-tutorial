d93f08bc90b593b2105187425ff48b9e2e181d2d
Jupeter <tensor.city@gmail.com>
2019-04-02 19:54:33 +0900

    102 - queries in firestore

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const productList = document.querySelector('#product-list');
-const form = document.querySelector('#add-product-form');
+const productForm = document.querySelector('#add-product-form');
+
+const cityList = document.querySelector('#city-list');
 
 // create element & render product
-function renderProduct(doc){
+function renderProduct(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let price = document.createElement('span');
+    let cross = document.createElement('div');
 
-    // li.setAttribute('data-id', doc.id);
+
+    li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     price.textContent = doc.data().price;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(price);
+    li.appendChild(cross);
 
     productList.appendChild(li);
+
+    // deleting data
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('products').doc(id).delete();
+    });
 }
 
 // getting data = promise
-db.collection('products').get().then(snapshot => {
+db.collection('products').where('price', '=', 100).get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         console.log(doc.data())
         renderProduct(doc);
@@ -26,12 +39,74 @@ db.collection('products').get().then(snapshot => {
 });
 
 // saving data
-form.addEventListener('submit', (e) => {
+productForm.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('products').add({
-        name: form.name.value,
-        price: form.price.value
+        name: productForm.name.value,
+        price: productForm.price.value
     });
-    form.name.value = '';
-    form.price.value = '';
+    productForm.name.value = '';
+    productForm.price.value = '';
+});
+
+// new collection
+var citiesRef = db.collection("cities");
+
+citiesRef.doc("SF").set({
+    name: "San Francisco", state: "CA", country: "USA",
+    capital: false, population: 860000,
+    regions: ["west_coast", "norcal"] });
+citiesRef.doc("LA").set({
+    name: "Los Angeles", state: "CA", country: "USA",
+    capital: false, population: 3900000,
+    regions: ["west_coast", "socal"] });
+citiesRef.doc("DC").set({
+    name: "Washington, D.C.", state: null, country: "USA",
+    capital: true, population: 680000,
+    regions: ["east_coast"] });
+citiesRef.doc("TOK").set({
+    name: "Tokyo", state: null, country: "Japan",
+    capital: true, population: 9000000,
+    regions: ["kanto", "honshu"] });
+citiesRef.doc("BJ").set({
+    name: "Beijing", state: null, country: "China",
+    capital: true, population: 21500000,
+    regions: ["jingjinji", "hebei"] });
+
+
+// create element & render product
+function renderCity(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let state = document.createElement('span');
+    let country = document.createElement('span');
+    let capital = document.createElement('span');
+    let population = document.createElement('span');
+    let regions = document.createElement('span');
+
+
+    name.textContent = doc.data().name;
+    state.textContent = doc.data().state;
+    country.textContent = doc.data().country;
+    capital.textContent = doc.data().capital;
+    population.textContent = doc.data().population;
+    regions.textContent = doc.data().regions;
+
+    li.appendChild(name);
+    li.appendChild(state);
+    li.appendChild(country);
+    li.appendChild(capital);
+    li.appendChild(population);
+    li.appendChild(regions);
+
+    cityList.appendChild(li);
+
+}
+
+// getting data = promise
+db.collection('cities').get().then(snapshot => {
+    console.log(snapshot.docs)
+    snapshot.docs.forEach(doc => {
+        renderCity(doc);
+    });
 });
